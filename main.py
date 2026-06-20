feb515c184adf57854871f8988297f3ed66daa20
promtengineering1c-create <promtengineering1c@gmail.com>
2026-06-20 17:23:23 +0500

    анализ файла dz.csv

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import pandas as pd
-df = pd.read_csv('benchmark_scores.csv')
+# df = pd.read_csv('benchmark_scores.csv')
 
-print(df.info())
-print('----------------------------------     ----------------------------------')
-print(df.describe())
-print('----------------------------------     ----------------------------------')
-print(df.head())
+# print(df.info())
+# print('----------------------------------     ----------------------------------')
+# print(df.describe())
+# print('----------------------------------     ----------------------------------')
+# print(df.head())
+
+df = pd.read_csv('dz.csv')
+
+# print(df.info())
+
+group = df.groupby('City')['Salary'].mean()
+
+print(group)
