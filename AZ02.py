4d975273d5e76b48b4a7dc312ae51e417e408d82
promtengineering1c-create <promtengineering1c@gmail.com>
2026-06-21 17:10:46 +0500

    Add extra hw

---
+++ AZ02.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import random
+
+names = ["Иван", "Мария", "Алексей", "Анна", "Дмитрий", "Екатерина", "Сергей", "Ольга", "Михаил", "Елена"]
+subjects = ["Математика", "Физика", "Информатика", "История", "Литература"]
+
+possible_grades = [2, 3, 4, 5]
+chances = [1, 2, 5, 3]
+
+data = {"Name": names}
+for subject in subjects:
+    data[subject] = random.choices(possible_grades, weights=chances, k=10)
+
+df = pd.DataFrame(data)
+
+subjects_df = df[subjects]
+
+mean_grades = subjects_df.mean()
+median_grades = subjects_df.median()
+
+print("--- Студенты и оценки ---")
+print(df.to_string(index=False))
+
+print("\n--- Средняя оценка по каждому предмету ---")
+print(mean_grades)
+
+print("\n--- Медианная оценка по каждому предмету ---")
+print(median_grades)
+
+Q1_math = df['Математика'].quantile(0.25)
+print("\n--- Q1 по математике ---")
+print(Q1_math)
+
+Q3_math = df['Математика'].quantile(0.75)
+print("\n--- Q3 по математике ---")
+print(Q3_math)
+
+IQR_math = Q3_math - Q1_math
+print("\n--- IQR по математике ---")
+print(IQR_math)
