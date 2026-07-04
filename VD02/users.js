450cbfed408d05ec85967b5f5ce88ca324e81f7a
promtengineering1c-create <promtengineering1c@gmail.com>
2026-07-04 15:00:34 +0500

    добавлена директория VD02

---
+++ VD02/users.js
@@ -0,0 +1,23 @@
+let usersData = [
+    {
+        photo: "Pictures/papa.webp",
+        fio: "Папа Барбоскин",
+        email: "papa@barboskiny.ru",
+        birthdate: "15 мая 1978",
+        city: "Пёсбург"
+    },
+    {
+        photo: "Pictures/liza.webp",
+        fio: "Лиза Барбоскина",
+        email: "liza.star@barboskiny.ru",
+        birthdate: "12 апреля 2012",
+        city: "Пёсбург"
+    },
+    {
+        photo: "Pictures/gena.webp",
+        fio: "Гена Барбоскин",
+        email: "gena.science@barboskiny.ru",
+        birthdate: "1 сентября 2008",
+        city: "Пёсбург"
+    }
+];
